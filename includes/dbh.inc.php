a62ed42245fe947983eb25756ca7860f4ab1503d
Kingsley Chu <kingsleychu0730@hotmail.com>
2020-04-01 20:48:39 -0400

    initial commit, added default php

---
+++ includes/dbh.inc.php
@@ -0,0 +1,8 @@
+<?php
+    $dbServerName = "localhost:8080";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbName = "animalrescue";
+    
+    $conn = mysqli_connect($dbServerName, $dbUsername, $dbPassword, $dbName);
+
