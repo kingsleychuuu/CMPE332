92ddb5a1cc6591787c25b9607abd4a414e42c1be
Kingsley Chu <kingsleychu0730@hotmail.com>
2020-04-08 06:27:45 -0400

    finished donations

--- classes/getDonations.php
+++ classes/getDonations.php
@@ -5,13 +5,13 @@ $pwd = "";
 $dbname = "animalrescue";
 $dsn = 'mysql:host=' . $host . ';dbname=' . $dbname;
 $pdo = new PDO($dsn, $user, $pwd);
-$sql = "SELECT * FROM spcabranch";
+$sql = "SELECT * FROM donations";
 $stmt = $pdo->query($sql);
 $stmt->execute();
 $res = $stmt->fetchAll();
 
 //$rescuerName = $_POST['submit'];
-$submitSql = "SELECT * FROM animals WHERE spcaBranchName = ?";
+$submitSql = "SELECT * FROM donations WHERE organizationName = ? AND YEAR(donationDate) = 2018";
 $submitStmt = $pdo->prepare($submitSql);
 $submitStmt->execute([$rescuerName]);
 $submitRes = $submitStmt->fetchAll();
@@ -19,14 +19,14 @@ $submitRes = $submitStmt->fetchAll();
 
 <html>
 <center>
-	<h1>GETTING THE ANIMALSSS WOOOOOO</h1>
+	<h1>GETTING THE DONATIONS WOOOOOOOOO</h1>
 	<h2>asdfasdfasdfasdfasdfas</h2>
 	<label>spcabranches:
 		<form action = "php.php" method = "post">
 		<select>
 			<option> --asdfsd-- </option>
 			<?php foreach ($res as $output) { ?>
-			<option> <?php echo $output['name'] ?> </option>
+			<option> <?php echo $output['donor'] ?> </option>
 			<?php } ?>
 		</select>
 		</form>
