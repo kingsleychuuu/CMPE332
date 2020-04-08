707b768e087b763a01c783927aed920597b85273
Shiningingy <56605710+Shiningingy@users.noreply.github.com>
2020-04-08 08:21:26 -0400

    Add files via upload

---
+++ index_selection.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- Zili Luo -->	
+	<head>
+    	<meta charset="UTF-8">
+    	<?php include_once "functions.php" ?>
+    	<meta name = "viewport" content="width=device-width, initial-scale=1"/>
+    	<!-- jQuery 1.7.2+ or Zepto.js 1.0+ -->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    	<title> CISC332 </title>
+	</head>
+	<body>
+		<SECTION>
+			<?php
+			print_selection()
+			?>
+		</SECTION>
+	</body>
+
+</html>
